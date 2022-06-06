2760dd7e409f20b5985159fb83fd9a7aff705ff0
Mike Jensen <jensen.mike57@gmail.com>
2022-06-06 11:26:29 +0200

    fix undefined pkg log bug

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -6,13 +6,21 @@ interface Step {
 }
 
 export const logStep = (step: Step) => {
-  const progress = `\x1b[35m[${step.pkgId}] \x1b[34m[${step.n}/${step.n_total}]\x1b[0m`;
   const msg = `\x1b[32m${step.message}\x1b[0m`;
+  const progress = step.pkgId
+    ? `\x1b[35m[${step.pkgId}] \x1b[34m[${step.n}/${step.n_total}]\x1b[0m`
+    : `\x1b[34m[${step.n}/${step.n_total}]\x1b[0m`;
+
   console.log(progress, msg);
 };
 
 export const logSubStep = (step: Step) => {
-  const progress = `\x1b[34m[${step.n}/${step.n_total}]\x1b[0m`;
   const msg = `\x1b[32m${step.message}\x1b[0m`;
-  console.log(new Array(`[${step.pkgId}]`.length + 1).join(' '), progress, msg);
+  const progress = `\x1b[34m[${step.n}/${step.n_total}]\x1b[0m`;
+
+  console.log(
+    step.pkgId ? new Array(`[${step.pkgId}]`.length + 1).join(' ') : `   `,
+    progress,
+    msg,
+  );
 };
