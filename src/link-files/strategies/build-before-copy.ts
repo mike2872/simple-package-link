0c481f4aacca566e4367bec7ad5eff91c8974897
Mike Jensen <jensen.mike57@gmail.com>
2022-09-07 12:13:53 +0200

    show output on debug true

--- src/link-files/strategies/build-before-copy.ts
+++ src/link-files/strategies/build-before-copy.ts
@@ -1,16 +1,18 @@
 import { childProcessSync } from '../../helpers/child-process';
 import copyFile from '../../helpers/copy-file';
 import forEachFile from '../../helpers/for-each-file';
+import { getConfig } from '../../helpers/get-config';
 import determinePathTarget from '../determine-path-target';
 
 export default async function buildBeforeCopy(pkg: LinkedPackage) {
+  const config = await getConfig();
   const buildOptions = pkg.strategy.options?.build;
   if (!buildOptions) throw new Error('No build options specified');
 
   childProcessSync(buildOptions.cmd, {
     args: buildOptions.args,
     cwd: pkg.src.root,
-    type: 'silent',
+    type: config.debug ? 'inherit' : 'silent',
   });
 
   forEachFile(buildOptions.outDir, file => {
