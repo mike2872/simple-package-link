6bb0cb3d861c50fb4b66a33b5381e0f1f3119efa
Mike Jensen <jensen.mike57@gmail.com>
2023-04-08 09:30:45 +0200

    chore(root): delete test package

--- packages/test-package/.lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
-  '*.{js,jsx,ts,tsx}': 'eslint --fix',
-  '*.{html,js,json,jsx,ts,tsx,css,md,yaml}': 'prettier --write',
-};
