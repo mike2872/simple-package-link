99da8024c3bbcbbfe4d8eceb74f86bfd22f2db48
Mike Jensen <jensen.mike57@gmail.com>
2023-04-13 11:24:53 +0200

    resolve glob async

--- packages/utils/src/resolve-glob.ts
+++ packages/utils/src/resolve-glob.ts
@@ -1,10 +1,15 @@
 import { glob } from 'glob';
 
-export default function resolveGlob(workspaceFolder: string, globExp: string) {
-  return glob(globExp, {
+export default async function resolveGlob(
+  workspaceFolder: string,
+  globExp: string,
+) {
+  const paths = await glob(globExp, {
     cwd: workspaceFolder,
     root: workspaceFolder,
     absolute: true,
     dot: true,
   });
+
+  return paths;
 }
